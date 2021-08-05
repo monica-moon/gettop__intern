9fc7c93937103ae4a67c83e450ee6524eda52dd0
monica-moon <abrmonica.moon@gmail.com>
2021-08-05 13:06:23 -0700

    Login

---
+++ pages/base_page/base_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Page:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, *locator):
+        self.driver.find_element(*locator).click()
+
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
+
+
+
+
+    def wait_for_form_appear(self, *locator):
+        self.driver.wait.until(EC.presence_of_element_located(locator))
+
+
